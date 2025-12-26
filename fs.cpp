601cbe5f98e1d014b9f1500f821cad6112052294
Samir Akhalil <saaa21@student.bth.se>
2025-12-27 00:01:49 +0100

    Initial commit: Lab 2 filesystem

---
+++ fs.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include "fs.h"
+
+FS::FS()
+{
+    std::cout << "FS::FS()... Creating file system\n";
+}
+
+FS::~FS()
+{
+
+}
+
+// formats the disk, i.e., creates an empty file system
+int
+FS::format()
+{
+    std::cout << "FS::format()\n";
+    // 1. Markera alla FAT-poster som fria
+    for (int i = 0; i < BLOCK_SIZE / 2; i++) {
+        fat[i] = FAT_FREE;
+    }
+
+    // 2. Reservera root och FAT-block
+    fat[ROOT_BLOCK] = FAT_EOF;
+    fat[FAT_BLOCK]  = FAT_EOF;
+
+    // 3. Skriv FAT till disk
+    disk.write(FAT_BLOCK, (uint8_t*) fat);
+
+    // 4. Skapa tom root directory
+    dir_entry empty_dir[BLOCK_SIZE / sizeof(dir_entry)];
+    memset(empty_dir, 0, sizeof(empty_dir));
+    disk.write(ROOT_BLOCK, (uint8_t*) empty_dir);
+
+    // 5. Sätt current directory till root
+    cwd_blk = ROOT_BLOCK;
+    cwd_path.clear();
+    return 0;
+}
+
+// create <filepath> creates a new file on the disk, the data content is
+// written on the following rows (ended with an empty row)
+int
+FS::create(std::string filepath)
+{
+    std::cout << "FS::create(" << filepath << ")\n";
+    return 0;
+}
+
+// cat <filepath> reads the content of a file and prints it on the screen
+int
+FS::cat(std::string filepath)
+{
+    std::cout << "FS::cat(" << filepath << ")\n";
+    return 0;
+}
+
+// ls lists the content in the currect directory (files and sub-directories)
+int
+FS::ls()
+{
+    std::cout << "FS::ls()\n";
+    return 0;
+}
+
+// cp <sourcepath> <destpath> makes an exact copy of the file
+// <sourcepath> to a new file <destpath>
+int
+FS::cp(std::string sourcepath, std::string destpath)
+{
+    std::cout << "FS::cp(" << sourcepath << "," << destpath << ")\n";
+    return 0;
+}
+
+// mv <sourcepath> <destpath> renames the file <sourcepath> to the name <destpath>,
+// or moves the file <sourcepath> to the directory <destpath> (if dest is a directory)
+int
+FS::mv(std::string sourcepath, std::string destpath)
+{
+    std::cout << "FS::mv(" << sourcepath << "," << destpath << ")\n";
+    return 0;
+}
+
+// rm <filepath> removes / deletes the file <filepath>
+int
+FS::rm(std::string filepath)
+{
+    std::cout << "FS::rm(" << filepath << ")\n";
+    return 0;
+}
+
+// append <filepath1> <filepath2> appends the contents of file <filepath1> to
+// the end of file <filepath2>. The file <filepath1> is unchanged.
+int
+FS::append(std::string filepath1, std::string filepath2)
+{
+    std::cout << "FS::append(" << filepath1 << "," << filepath2 << ")\n";
+    return 0;
+}
+
+// mkdir <dirpath> creates a new sub-directory with the name <dirpath>
+// in the current directory
+int
+FS::mkdir(std::string dirpath)
+{
+    std::cout << "FS::mkdir(" << dirpath << ")\n";
+    return 0;
+}
+
+// cd <dirpath> changes the current (working) directory to the directory named <dirpath>
+int
+FS::cd(std::string dirpath)
+{
+    std::cout << "FS::cd(" << dirpath << ")\n";
+    return 0;
+}
+
+// pwd prints the full path, i.e., from the root directory, to the current
+// directory, including the currect directory name
+int
+FS::pwd()
+{
+    std::cout << "FS::pwd()\n";
+    return 0;
+}
+
+// chmod <accessrights> <filepath> changes the access rights for the
+// file <filepath> to <accessrights>.
+int
+FS::chmod(std::string accessrights, std::string filepath)
+{
+    std::cout << "FS::chmod(" << accessrights << "," << filepath << ")\n";
+    return 0;
+}
